17ccf2c7827b34a8005ffd97b645238885909c33
sandakin <sandakin@ayra.com.au>
2019-12-22 16:55:21 +0530

    wsgi file edit

--- scoreboard_back/wsgi.py
+++ scoreboard_back/wsgi.py
@@ -8,8 +8,13 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+
+sys.path.append('/var/www/scoreboad_back')
+# adjust the Python version in the line below as needed
+sys.path.append('/var/www/scoreboad_back/venv/lib/python3.6/site-packages')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scoreboard_back.settings')
 
