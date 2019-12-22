02cb405055963a665b94c8c3697e4ae7fa594747
sandakin <sandakin@ayra.com.au>
2019-12-22 13:05:12 +0530

    static url added

--- scoreboard_back/urls.py
+++ scoreboard_back/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf.urls import url, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'', include('api.urls')),
     url(r'^o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
