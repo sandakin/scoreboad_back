f5e92220910578daec89854d9357616df4e4956c
sandakin <sandakin@ayra.com.au>
2019-12-24 12:25:28 +0530

    bugs fixed

--- api/views.py
+++ api/views.py
@@ -45,15 +45,15 @@ class TeamViewSet(viewsets.ModelViewSet):
     queryset = Team.objects.all()
     serializer_class = TeamSerializer
 
-    def get_queryset(self):
-        team_list = sorted(Team.objects.all(), key=lambda t: t.avg_score, reverse=True)
-        team_list = [obj.id for obj in team_list]
-        return Team.objects.filter(id__in=team_list)
+    # def get_queryset(self):
+    #     team_list = sorted(Team.objects.all(), key=lambda t: t.avg_score, reverse=True)
+    #     team_list = [obj.id for obj in team_list]
+    #     return Team.objects.filter(id__in=team_list)
 
-    # def list(self, request, *args, **kwargs):
-    #     team_list = super().list(request, args, kwargs)
-    #     team_list.data = sorted(team_list.data, key=lambda k: k['avg_score'], reverse=True)
-    #     return team_list
+    def list(self, request, *args, **kwargs):
+        team_list = super().list(request, args, kwargs)
+        team_list.data = sorted(team_list.data, key=lambda k: k['avg_score'], reverse=True)
+        return team_list
 
 
 class GameScoreViewSet(viewsets.ModelViewSet):
